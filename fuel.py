a4b7e3e45a802ae310873314716c820e7dc719de
Abdulj28 <Aj591@kent.ac.uk>
2024-12-02 04:56:37 +0000

    comments added

--- fuel.py
+++ fuel.py
@@ -3,52 +3,53 @@
 def main():
     try:
         x = int(input("What is X? "))
-        y = int(input("What is Y? "))
+        y = int(input("What is Y? ")) #gets user input x and y as integers
         
-        fraction = x / y
+        fraction = x / y #saves x divided by y as a variable called fraction
 
-        if (x>y):
+        if (x>y): #checks if x is greater than y
          print ("x can't be greater than y")
    
         else:
-            percentage = convert(fraction)
-            result = gauge(percentage)
-            print(result)
+            percentage = convert(fraction) #calculates the percentage using the fraction
+            result = gauge(percentage) #determines whether F,E or a rounded perctentage willl be given
+            print(result) #prints the final result
 
 
 
     except ValueError:
-        print("error, type an integer")
+        print("error, type an integer") # the error message will occur if anything but an integer is printed
          
     except ZeroDivisionError:
-        result = "error, Cannot divide by zero."
-        print(result)
+        print("error, Cannot divide by zero.") # error is printed if integer is divided by 0
+        
+        
+   
 
 
         
 
 
 def convert(fraction):
-    return round(fraction*100)
+    return round(fraction*100) #returns a number that is a fraction into a rounded percentage
 
 
 
 
 def gauge(percentage):
 
-    if percentage <= 1:
+    if percentage <= 1: # if percentahe is equal to or less than 1 E is printed
         return "E"
-    elif percentage >= 99:
+    elif percentage >= 99: # if percentage is equal to or more than 99 then F is printed
         return "F"
     else:
-        return f"{percentage}%"
+        return f"{percentage}%" # any integer between that is printed as a percnetage
 
 
 
 
-if __name__ == "__main__":
-    main()
-
+if __name__ == "__main__": 
+    main() # runs the main part of the code
 
 
 
--- test_fuel.py
+++ test_fuel.py
@@ -1,13 +1,15 @@
 import pytest
-from fuel import convert, gauge
+from fuel import convert, gauge #imports functions from fuel.py
 
 def test_convert():
+    # converts fractions into percentages. this is ran throuh the fuel.py to check if all the answers are what they should be.
     assert convert(0.25) == 25
     assert convert(0.75) == 75
     assert convert(0) == 0
     assert convert(1) == 100
 
 def test_gauge():
+    # tests if the right vvariable will be printed out depending on the percentages
     assert gauge(0) == "E"
     assert gauge(1) == "E"
     assert gauge(69) == "69%"
@@ -15,5 +17,7 @@ def test_gauge():
     assert gauge(100) == "F"
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+    
+    # runs the tests using pytest
     pytest.main()
